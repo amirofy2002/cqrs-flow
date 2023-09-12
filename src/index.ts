4336f9ed13979f3f28e30c473ff891bb5cfbad8e
Amir Mehrabi <amir.mehrabi.j@gmail.com>
2023-09-12 22:58:07 +0330

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { EventBus, CommandBus } from "./services";
 import { CommandHandler, EventHandler } from "./services/decorators";
+import { ICommand, ICommandHandler, IEvent, IEventHandler } from "./core/types";
+
 export { EventBus, CommandBus, CommandHandler, EventHandler };
+export { ICommand, ICommandHandler, IEvent, IEventHandler };
