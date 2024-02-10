77174bf39abba5bc4e938ee953f13fe3b96d98d4
Amir Mehrabi <amir.mehrabi.j@gmail.com>
2024-02-10 20:56:42 +0330

    fix: event

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { CommandHandler, EventHandler } from "./services/decorators";
 import { ICommand, ICommandHandler, IEvent, IEventHandler } from "./core/types";
 import { ExecutionFailedEvent } from "./core/events/execution-failed.event";
 
-export { EventBus, CommandBus, CommandHandler, EventHandler };
+export {
+  EventBus,
+  CommandBus,
+  CommandHandler,
+  EventHandler,
+  ExecutionFailedEvent,
+};
 export { ICommand, ICommandHandler, IEvent, IEventHandler };
-export { ExecutionFailedEvent };
