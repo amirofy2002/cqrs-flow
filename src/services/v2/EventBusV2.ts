ec4b780300ae5a86a2ad95338c3c7dfff5b4db4a
Amir Mehrabi <amir.mehrabi.j@gmail.com>
2025-08-24 11:48:10 +0330

    fix: change reject handler for buses

--- src/services/v2/EventBusV2.ts
+++ src/services/v2/EventBusV2.ts
@@ -2,25 +2,30 @@ import { Subject } from "rxjs";
 import { IEvent, IEventHandler } from "../../core/types";
 
 export class EventBusV2 {
-  private bus = new Subject<{ event: IEvent; resolve: (result: any) => any }>();
+  private bus = new Subject<{
+    event: IEvent;
+    resolve: (result: any) => any;
+    reject: (err: string) => void;
+  }>();
   private static handlerCache = new Map<string, IEventHandler<IEvent>>();
 
   constructor() {
     this.init();
   }
   init() {
-    console.log("initialized event bus...");
-    this.bus.subscribe(async ({ event, resolve }) => {
-      const commandName = event.constructor.name;
-      const handlerInstance = EventBusV2.handlerCache.get(commandName);
+    console.log("initializing event bus...");
+    this.bus.subscribe(({ event, resolve, reject }) => {
+      const eventName = event.constructor.name;
+      const handlerInstance = EventBusV2.handlerCache.get(eventName);
       if (!handlerInstance) {
-        console.error("no handler defined for this event ", commandName);
+        console.error("no handler defined for this event ", eventName);
+        reject(`no event handler found for this event: ${eventName}`);
         return;
       }
 
-      const result = await handlerInstance.handle(event);
-      return resolve(result);
+      handlerInstance.handle(event).catch(reject).then(resolve);
     });
+    console.log("initialized event bus.");
   }
 
   handle<K>(event: IEvent): Promise<K> {
@@ -28,18 +33,18 @@ export class EventBusV2 {
       console.error("Invalid event: must implement IEvent. Received:", event);
       return Promise.reject("invalid event");
     }
-    return new Promise<K>((resolve) => {
-      this.bus.next({ event: event, resolve });
+    return new Promise<K>((resolve, reject) => {
+      this.bus.next({ event: event, resolve, reject });
     });
   }
 
   public static register(event: string, handler: IEventHandler<IEvent>): void {
     if (!handler || typeof handler.handle !== "function") {
-      console.error("Invalid handler: must implement handle method.");
+      console.error("Invalid event handler: must implement handle method.");
       return;
     }
 
     EventBusV2.handlerCache.set(event, handler);
-    console.log(`Handler registered for event: [${event}]`);
+    console.log(`Event Handler registered for event: [${event}]`);
   }
 }
--- src/services/v2/QueryBusV2.ts
+++ src/services/v2/QueryBusV2.ts
@@ -9,7 +9,11 @@ import { IQueryHandler } from "../../core/types/IQueryHandler";
 import { IQuery } from "../../core/types/IQuery";
 
 export class QueryBusV2 {
-  private bus = new Subject<{ query: IQuery; resolve: (result: any) => any }>();
+  private bus = new Subject<{
+    query: IQuery;
+    resolve: (result: any) => any;
+    reject: (err: string) => void;
+  }>();
   private static handlerCache = new Map<string, IQueryHandler<IQuery>>();
 
   constructor() {
@@ -17,16 +21,16 @@ export class QueryBusV2 {
   }
   init() {
     console.debug("initialized query bus...");
-    this.bus.subscribe(async ({ query, resolve }) => {
+    this.bus.subscribe(({ query, resolve, reject }) => {
       const queryName = query.constructor.name;
       const handlerInstance = QueryBusV2.handlerCache.get(queryName);
       if (!handlerInstance) {
         console.error("no handler defined for this query ", queryName);
+        reject(`no query handler found for this event: ${queryName}`);
         return;
       }
 
-      const result = await handlerInstance.run(query);
-      resolve(result);
+      handlerInstance.run(query).catch(reject).then(resolve);
     });
   }
 
@@ -35,18 +39,18 @@ export class QueryBusV2 {
       console.error("Invalid query: must implement IQuery. Received:", query);
       return Promise.reject("invalid query");
     }
-    return new Promise<K>((resolve) => {
-      this.bus.next({ query: query, resolve });
+    return new Promise<K>((resolve, reject) => {
+      this.bus.next({ query: query, resolve, reject });
     });
   }
 
   public static register(query: string, handler: IQueryHandler<IQuery>): void {
     if (!handler || typeof handler.run !== "function") {
-      console.error("Invalid handler: must implement run method.");
+      console.error("Invalid query handler: must implement run method.");
       return;
     }
 
     QueryBusV2.handlerCache.set(query, handler);
-    console.log(`Handler registered for query: [${query}]`);
+    console.log(`Query Handler registered for query: [${query}]`);
   }
 }
